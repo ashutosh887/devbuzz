425e719fccde0d33f2217b463f0469c198d2b5ca
Ashutosh Jha <ashutoshj887@gmail.com>
2025-04-28 20:06:05 +0530

    feat: add configs

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+const config = {
+  appName: "DevBuzz",
+  appTitle: "Where Developers Build, Share, and Grow.",
+  author: {
+    name: "@ashutosh887",
+    url: "https://www.linkedin.com/in/ashutosh887/",
+  },
+};
+
+export default config;
